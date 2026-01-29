c4e2717df0a2921ed680d701a27eb6c1552babaf
adamws <adamws@users.noreply.github.com>
2026-01-29 21:24:44 +0100

    Fix invalid close page warning on 'Download ZIP' click
    Fixes #50

--- src/components/PcbDownloadButton.vue
+++ src/components/PcbDownloadButton.vue
@@ -15,6 +15,8 @@ function handleDownload() {
     const link = document.createElement('a')
     link.href = url
     link.download = ''
+    link.target = '_blank'
+    link.rel = 'noopener noreferrer'
     link.click()
   }
 }
